cd282c4a0adcb1f247498180008b8c01995c489e
nanlei <36439285+nanlei2000@users.noreply.github.com>
2020-01-15 10:26:08 +0800

    ...

--- src/data.ts
+++ src/data.ts
@@ -2,7 +2,7 @@ interface Datum {
     name: string;
     date: string;
     university: string;
-    education: '硕士研究生' | '博士研究生';
+    education: '硕士' | '博士';
     link: [string, string][];
     major: '材料' | '计算机' | '凝聚态物理' | '医学' | '药理学' | '自动化' | '航天' | '机电';
     gender: '男' | '女';
@@ -18,7 +18,7 @@ const data: Datum[] = [
         name: '谭大伟',
         date: '2019-12-26',
         university: '南京邮电大学',
-        education: "硕士研究生",
+        education: "硕士",
         link: [
             ['如何看待南邮材料学院研究生不堪忍受导师压榨疑似于实验室自焚事件?',
                 'https://www.zhihu.com/question/364795757/answer/963617654']
@@ -30,7 +30,7 @@ const data: Datum[] = [
         name: '陈泽民',
         date: '2019-09-02',
         university: '华中科技大学',
-        education: "硕士研究生",
+        education: "硕士",
         link: [
             ['华科研究生跳楼身亡，留下7页自述问责导师',
                 'https://mp.weixin.qq.com/s/8I5g8yZKI8ymcK80bdg_8w']
@@ -42,7 +42,7 @@ const data: Datum[] = [
         name: '段付谋',
         date: '2019-06-03',
         university: '南京大学',
-        education: "博士研究生",
+        education: "博士",
         link: [
             ['南大博士跳江自杀！家属称或与导师指责有关！',
                 'http://www.sohu.com/a/323405903_120066380']
@@ -54,7 +54,7 @@ const data: Datum[] = [
         name: '胡坤',
         date: '2019-01-20',
         university: '华南理工大学',
-        education: "博士研究生",
+        education: "博士",
         link: [
             ['如何看待华南理工大学材料系博士生胡坤猝死事件？',
                 'https://www.zhihu.com/question/310301027']
@@ -66,7 +66,7 @@ const data: Datum[] = [
         name: '陆经纬',
         date: '2018-12-13',
         university: '同济大学',
-        education: "硕士研究生",
+        education: "硕士",
         link: [
             ['如何看待同济大学医学院研究生陆经纬疑似因与导师陆琰君不合跳楼自尽？',
                 'https://www.zhihu.com/question/308045928']
@@ -78,7 +78,7 @@ const data: Datum[] = [
         name: '田苗',
         date: '2018-11-13',
         university: '华南理工大学',
-        education: "硕士研究生",
+        education: "硕士",
         link: [
             ['如何看待华南理工大学田苗坠楼事件？',
                 'https://www.zhihu.com/question/299047162']
@@ -90,7 +90,7 @@ const data: Datum[] = [
         name: '陶崇园',
         date: '2018-03-26',
         university: '武汉理工大学',
-        education: "硕士研究生",
+        education: "硕士",
         link: [
             ['陶崇园事件',
                 'https://zh.wikipedia.org/wiki/%E9%99%B6%E5%B4%87%E5%9B%AD%E4%BA%8B%E4%BB%B6']
@@ -104,7 +104,7 @@ const data: Datum[] = [
         name: '杨宝德',
         date: '2017-12-25',
         university: '西安交通大学',
-        education: '博士研究生',
+        education: '博士',
         link: [
             ['如何评价西安交大的博导周筠「逼死」博士生杨宝德？',
                 'https://www.zhihu.com/question/265236935']
@@ -116,7 +116,7 @@ const data: Datum[] = [
         name: '周旭',
         date: '2017-06-10',
         university: '西北工业大学',
-        education: '博士研究生',
+        education: '博士',
         link: [
             ['如何看待西北工业大学6月10日晚一博士生坠楼身亡？',
                 'https://www.zhihu.com/question/60977461']
@@ -128,7 +128,7 @@ const data: Datum[] = [
         name: '姜东生',
         date: '2015-05-18',
         university: '中南大学',
-        education: '硕士研究生',
+        education: '硕士',
         link: [
             ['如何评价中南大学研究生答辩未通过跳楼自杀并留下五千字遗书？',
                 'https://www.zhihu.com/question/30607078'],
@@ -164,7 +164,7 @@ const tempTail = `
 </html>`;
 
 const body = `
-<p>在因魔鬼导师而自杀的这 ${data.length} 例事件中,男性占比 ${
+<p>在因魔鬼导师而自杀的这 ${data.length} 例事件中，男性占比 ${
     calculateRate(data.filter(v => v.gender === '男').length, data.length)
     },最多的专业是${calculateMost(data, 'major')}。</p>
 <table class="table 
